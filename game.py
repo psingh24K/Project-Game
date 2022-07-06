1a152f46cef561df2f5300481d9d967ee90b0c06
Prabhnoor Singh <prabhnoorsingh@MacBook-Pro.local>
2022-07-06 15:58:17 -0400

    getting started with tkinter

--- game.py
+++ game.py
@@ -1,14 +1,24 @@
 # tic-tac-toe game (code academy project)
-import tkinter
+from tkinter import *
+from tkinter import messagebox
+
+# create a object root of TK(tkinter) class
+root = Tk()
+
+# Call the title method from TK class using root as an object since it can access all the methods of tkinter class
+root.title("Tic-Tac-Toe Game")
+
+# Put icon in the top left of the widget using iconbitmap method from root object of TK class
+root.iconbitmap('/Users/prabhnoorsingh/Computing/Back End/Python/Project Game/title_icon.png')
+
+# Build our Buttons
 
 
 
-#This class will make the game board
-class TicTacToeBoard:
-  pass
 
 
-#this class will have the logic of the game
-class TicTacToeGame:
-  pass
 
+
+
+
+root.mainloop()
